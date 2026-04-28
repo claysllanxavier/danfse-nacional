5a74470d7164f0a5dc32b3da1b5a542de87fe717
Claysllan Xavier <claysllan@gmail.com>
2026-04-28 12:21:22 -0300

    🐛 Corrige geração do DTO com valor total como array no XML

--- src/Dto/TotTrib.php
+++ src/Dto/TotTrib.php
@@ -5,7 +5,7 @@ namespace DanfseNacional\Dto;
 readonly class TotTrib
 {
     public function __construct(
-        public string $vTotTrib = '',
+        public string|array $vTotTrib = '',
         public ?TotTribPercent $pTotTrib = null,
         public string $indTotTrib = '',
         public string $pTotTribSN = '',
